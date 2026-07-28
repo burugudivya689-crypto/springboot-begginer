7316da7ff9af144c31def28e1af7129f8ada4294
Divya <burugudivya689@email.com>
2026-07-28 14:30:21 +0530

    updates sb

---
+++ src/main/java/com/being_infinity/sprintboot_beginner/SprintbootBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.being_infinity.sprintboot_beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootBeginnerApplication.class, args);
+	}
+
+}
