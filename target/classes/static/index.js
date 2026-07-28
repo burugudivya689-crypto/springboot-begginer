ea9699443e9f903058a657fee6fdf215a85f9f9d
Divya <burugudivya689@email.com>
2026-07-28 22:51:48 +0530

    Created toggle card along with functionality.

---
+++ target/classes/static/index.js
@@ -0,0 +1,29 @@
+//alert("Hello from javaScript");
+// alert("Hello from javaScript");
+
+var users = [
+    {
+        "image" : "/images/john.png",
+        "name" : "John Doe",
+        "gender" : "Male"
+    },
+    {
+        "image" : "/images/jane.png",
+        "name" : "Jane Doe",
+        "gender" : "Female"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1) % users.length;
+    var userImage = document.getElementById("user-image");
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+
+    userImage.src = users[id].image;
+    userName.innerHTML = users[id].name;
+    userGender.innerHTML = users[id].gender;
+
+}
